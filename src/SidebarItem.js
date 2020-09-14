899cd51f078a35ee604caa15df15a82bbec95091
alza <alzadinaya@gmail.com>
2020-09-14 18:04:03 +0200

    photoeditor from git

---
+++ src/SidebarItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SidebarItem({name, active, handleClick}) {
+    return (
+        <button className={`sidebar-item ${active ? 'active' : ''}`}
+        onClick={handleClick}
+        >
+          {name}
+        </button>
+    )
+}
